5ca4611acf60da187ff92f130668edc2023b81d0
slizer432 <rayhanheruk@gmail.com>
2023-09-18 09:16:41 +0700

    first commit

---
+++ Latihan23.java
@@ -0,0 +1,7 @@
+public class Latihan23 {
+    
+    public static void main(String[] args) {
+        System.out.print("\nNama Saya Rayhan Heru Arifiansyah");
+        System.out.print("\nSaya adalah mahasiswa baru jurusan Teknologi Informasi Polinema");
+    }
+}
---
+++ Tugas.java
@@ -0,0 +1,12 @@
+public class Tugas {
+    
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama          : Rayhan Heru Arifiansyah");
+        System.out.println("absen         : 23");
+        System.out.println("kelas         : 1F");
+        System.out.println("Prodi         : D4 Teknik Informatika");
+        System.out.println("Jurusan       : Teknologi Informasi");
+        
+    }
+}
